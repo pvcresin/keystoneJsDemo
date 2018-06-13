9692700f45c5dfc841b042b0854341b36d335215
pvcresin <pvcresin0730@gmail.com>
2018-06-13 22:11:14 +0900

    add images

--- models/Entry.js
+++ models/Entry.js
@@ -9,6 +9,14 @@ var Entry = new keystone.List('Entry', {
 	map: { name: 'title' }
 })
 
+var myStorage = new keystone.Storage({
+	adapter: keystone.Storage.Adapters.FS,
+	fs: {
+		path: 'public/uploads',
+		publicPath: '/uploads'
+	}
+})
+
 Entry.add({
 	title: {
 		type: String,
@@ -20,11 +28,26 @@ Entry.add({
 	content: {
 		type: Types.Html,
 		wysiwyg: true
+	},
+	img: {
+		type: Types.File,
+		filename: function(item, file) {
+			return item.id + '.' + file.extension
+		},
+		format: function(item, file) {
+			return `<img src='uploads/${file.filename}' />`
+		},
+		storage: myStorage
+	},
+	location: {
+		type: Types.Location,
+		enableMapsAPI: true,
+		defaults: { country: 'Australia' }
 	}
 })
 
 /**
  * Registration
  */
-Entry.defaultColumns = 'title, content'
+Entry.defaultColumns = 'title, content, img'
 Entry.register()
